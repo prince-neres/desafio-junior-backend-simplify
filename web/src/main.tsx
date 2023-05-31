bdbfff4c0b3ada4c5f34666478dfc20bef0cb786
Prince Neres <prince84224960@gmail.com>
2023-05-31 13:39:05 -0300

    chore: Adicionando tailwind ao projeto

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./global.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <p>To-Do List</p>
+  </React.StrictMode>
+);
