492171fdb442f1523629d4d51e503e67221e76bc
Prince Neres <prince84224960@gmail.com>
2023-06-02 02:03:51 -0300

    feat: Adicionando docker para rodar aplicação

--- api/config.py
+++ api/config.py
@@ -8,8 +8,8 @@ class Config:
     ENVIRONMENT = getenv('ENVIRONMENT') or 'dev'
     SECRET_KEY = getenv('SECRET_KEY') or 'secret'
     SQLALCHEMY_DATABASE_URI = getenv(
-        'SQLALCHEMY_DATABASE_URI') if ENVIRONMENT == 'prod' else 'postgresql://user:password@localhost:5432/todolist'
+        'SQLALCHEMY_DATABASE_URI') if ENVIRONMENT == 'prod' else 'postgresql://teste:teste@db:5432/todolist'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = getenv('JWT_SECRET_KEY')
+    JWT_SECRET_KEY = getenv('JWT_SECRET_KEY') or 'jwt_secret'
     CLIENT_URL = getenv(
         'CLIENT_URL') if ENVIRONMENT == 'prod' else 'http://localhost:3000'
