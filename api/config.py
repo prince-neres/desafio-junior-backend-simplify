ba7d9f3df28b73cc09eb95becbf451b201a6deb5
Prince Neres <prince84224960@gmail.com>
2023-06-02 01:29:49 -0300

    fix: Alterando variáveis de ambiente

--- api/config.py
+++ api/config.py
@@ -5,10 +5,11 @@ load_dotenv()
 
 
 class Config:
-    ENVIRONMENT = getenv('ENVIRONMENT')
-    SECRET_KEY = getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = getenv('SQLALCHEMY_DATABASE_URI')
+    ENVIRONMENT = getenv('ENVIRONMENT') or 'dev'
+    SECRET_KEY = getenv('SECRET_KEY') or 'secret'
+    SQLALCHEMY_DATABASE_URI = getenv(
+        'SQLALCHEMY_DATABASE_URI') if ENVIRONMENT == 'prod' else 'postgresql://user:password@localhost:5432/todolist'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = getenv('JWT_SECRET_KEY')
     CLIENT_URL = getenv(
-        'CLIENT_URL') if ENVIRONMENT == 'prod' else 'http://localhost:5173'
+        'CLIENT_URL') if ENVIRONMENT == 'prod' else 'http://localhost:3000'
